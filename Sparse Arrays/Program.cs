78bb68b86db325dcbb414d428299b3baab48954a
ElhamDrg <elham.dargi@gmail.com>
2022-07-06 21:57:06 +0200

    Add project files.

---
+++ Sparse Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparse_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string> {"ab", "ab", "abc" };
+            List<string> queries = new List<string> {"ab","abc","bc"};
+            List<int> results = new List<int>();
+            foreach (var item in queries)
+            {
+                results.Add(strings.Count(x => x == item));
+            }
+            results.ForEach(Console.WriteLine);
+        }
+    }
+}
